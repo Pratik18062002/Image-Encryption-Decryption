eaf76d9165855fa4a8444e3d32940de7405a3e77
Pratik18062002 <109238090+Pratik18062002@users.noreply.github.com>
2024-01-29 19:27:43 +0530

    Add files via upload

---
+++ Main File/Encrypt.java
@@ -0,0 +1,18 @@
+
+import javax.swing.JTextField;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class Encrypt extends JTextField {
+
+    public Encrypt(int i) {
+    }
+    
+}
